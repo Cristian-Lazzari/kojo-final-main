f0a2305827059165caf6a5b8958dfc451b92736c
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-12-07 19:15:18 +0100

    asc

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -33,7 +33,7 @@ export default {
         <div class="home-left">
             <img src="../assets/img/sushibac.png" alt="" class="sushi">
         </div>
-        <div class="home-right shadow">
+        <div class="home-right ">
             <h1>SCOPRI LA VERA CUCINA ORIENTALE E PROVA IL NOSTRO SUSHI</h1>
             <p>SOLO SUL NOSTRO SITO UFFICIALE PUOI TROVARE IL NOSTRO MENU, PRENOTARE UN TAVOLO O ADDIRITTURA PRENOTARE IL
                 TUO ASPORTO</p>
